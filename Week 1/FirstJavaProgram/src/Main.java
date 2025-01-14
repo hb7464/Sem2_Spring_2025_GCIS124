4ae0e3df110332ed929c3d82cbc338f86a414395
hb7464 <hb7464@g.rit.edu>
2025-01-14 15:27:03 +0400

    s

--- Week 1/FirstJavaProgram/src/Main.java
+++ Week 1/FirstJavaProgram/src/Main.java
@@ -1,31 +1,58 @@
 public class Main {
+
+    public static double divide(int num1, int num2) {
+
+        return (double) num1 / num2;
+    }
+
+    public static double averg(int num1, int num2, int num3) {
+
+        return (double) (num1 + num2 + num3) /3;
+    }
+
     public static void main(String[] args) {
 
         // System.out.print("square of 5 is ");
         // System.out.println(Math.pow(5, 2));
-        int a = 5;
-        short s = 3;
-        byte b = 14;
-        long l = 25L;
-        int l1 = 25;
-        float f = 3.141434f;
-        double d = 3.134142141354678;
-        float ff = 34;
-        boolean ffi = true;
-        double ffd = 34L;
-        long ffl1 = (long)43.5;
+//        int a = 5;
+//        short s = 3;
+//        byte b = 14;
+//        long l = 25L;
+//        int l1 = 25;
+//        float f = 3.141434f;
+//        double d = 3.134142141354678;
+//        float ff = 34;
+//        boolean ffi = true;
+//        double ffd = 34L;
+//        long ffl1 = (long)43.5;
+//
+//        System.out.println(a);
+//        System.out.println(s);
+//        System.out.println(b);
+//        System.out.println(l);
+//        System.out.println(l1);
+//        System.out.println(f);
+//        System.out.println(d);
+//        System.out.println(ff);
+//        System.out.println(ffi);
+//        System.out.println(ffd);
+//        System.out.println(ffl1);
 
-        System.out.println(a);
-        System.out.println(s);
-        System.out.println(b);
-        System.out.println(l);
-        System.out.println(l1);
-        System.out.println(f);
-        System.out.println(d);
-        System.out.println(ff);
-        System.out.println(ffi);
-        System.out.println(ffd);
-        System.out.println(ffl1);
+//        long asdf = 51L + 3;
+//        float asdfe = 51L + 3;
+//        int asd = 4/3;
+//        double asdeff = 4/(double)3;
+//
+//        System.out.println(asdf);
+//        System.out.println(asdfe);
+//        System.out.println(asd);
+//        System.out.println(asdeff);
+
+        System.out.println(averg(3,3,5));
+        System.out.println(averg(20,14,15));
+        System.out.println(averg(100,34,50));
+// create function that receives to int numbers and
+// returns their average as a double
 
     }
 }
