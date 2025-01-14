826124e3021206004eea7eaea1c513adacce908f
hb7464 <hb7464@g.rit.edu>
2025-01-14 10:56:47 +0400

    sds

--- Week 1/FirstJavaProgram/src/Main.java
+++ Week 1/FirstJavaProgram/src/Main.java
@@ -1,8 +1,31 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("square of 5 is ");
-        System.out.println(Math.pow(5, 2));
+        // System.out.print("square of 5 is ");
+        // System.out.println(Math.pow(5, 2));
+        int a = 5;
+        short s = 3;
+        byte b = 14;
+        long l = 25L;
+        int l1 = 25;
+        float f = 3.141434f;
+        double d = 3.134142141354678;
+        float ff = 34;
+        boolean ffi = true;
+        double ffd = 34L;
+        long ffl1 = (long)43.5;
+
+        System.out.println(a);
+        System.out.println(s);
+        System.out.println(b);
+        System.out.println(l);
+        System.out.println(l1);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(ff);
+        System.out.println(ffi);
+        System.out.println(ffd);
+        System.out.println(ffl1);
 
     }
 }
