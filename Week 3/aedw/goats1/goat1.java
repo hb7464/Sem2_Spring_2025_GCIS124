d58d00a44a304554c086cea34a3d32384e9678cc
hb7464 <hb7464@g.rit.edu>
2025-01-28 11:02:45 +0400

    dsad

---
+++ Week 3/aedw/goats1/goat1.java
@@ -0,0 +1,5 @@
+package Sem2_Spring_2025_GCIS124.Week 3.aedw.goats1;
+
+public class goat1 {
+
+}
--- Week 3/aedw/src/filemaine12313.java
+++ Week 3/aedw/src/filemaine12313.java
@@ -1,7 +1,5 @@
-public class azqwsxedcrfvtgbyhnujm {
+public class filemaine12313 {
     public static void main(String[] args) {
-
         System.out.println("Hello, World!");
-        
     }
 }
