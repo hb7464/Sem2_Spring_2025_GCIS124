88c7309326dab3ee696f5b86141bcff4684bc8ac
hb7464 <hb7464@g.rit.edu>
2025-02-04 09:35:45 +0400

    zsxdrcfvgbh

--- Assignments/Assignment 1/src/MCQQuestion.java
+++ Assignments/Assignment 1/src/MCQQuestion.java
@@ -79,6 +79,7 @@ public class MCQQuestion {
     }
 
     // The main class in which we create an MCQQuestion object
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
 
         // Initializing the variables for an object
