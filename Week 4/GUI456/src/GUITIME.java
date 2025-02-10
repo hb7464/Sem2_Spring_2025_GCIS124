0524b3d89c620166c6b92c7b670463963fec296d
hb7464 <hb7464@g.rit.edu>
2025-02-10 11:46:23 +0400

    hubhbhi

--- Week 4/GUI456/src/GUITIME.java
+++ Week 4/GUI456/src/GUITIME.java
@@ -1,6 +1,6 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
-// import javafx.geometry.Pos;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
@@ -22,20 +22,20 @@ public class GUITIME extends Application{
     @Override
     public void start(Stage stage) throws Exception{
 
-        Label label1 = labelFactory("First Label",Color.BLACK,Color.BLUE);
+        Label label1 = labelFactory("First Label",Color.BLACK,Color.BLUE,Color.AQUA);
 
-        Label label2 = labelFactory("Second Label",Color.BLACK,Color.BLUE);
+        Label label2 = labelFactory("Second Label",Color.BLACK,Color.BLUE,Color.AQUA);
 
         HBox hb = new HBox();
         hb.getChildren().addAll(label1,label2);
 
         BorderPane pane = new BorderPane();
 
-        pane.setTop(labelFactory("dwdad", Color.BLUE, Color.BLACK));
-        pane.setBottom(labelFactory("dwd", Color.BLUE, Color.GREEN));
-        pane.setLeft(labelFactory("dd", Color.BLUE, Color.GRAY));
-        pane.setRight(labelFactory("dwwwdad", Color.BLUE, Color.RED));
-        pane.setCenter(labelFactory("deewdad", Color.BLUE, Color.YELLOW));
+        pane.setTop(labelFactory("label 1", Color.WHITE, Color.BLACK, Color.AQUA));
+        pane.setBottom(labelFactory("label 2", Color.WHITE, Color.GREEN, Color.AQUA));
+        pane.setLeft(labelFactory("label 3", Color.WHITE, Color.GRAY, Color.AQUA));
+        pane.setRight(labelFactory("label 4", Color.WHITE, Color.RED, Color.AQUA));
+        pane.setCenter(labelFactory("label 5", Color.WHITE, Color.PURPLE, Color.AQUA));
 
         Scene scene = new Scene(pane);
         stage.setScene(scene);
@@ -43,17 +43,22 @@ public class GUITIME extends Application{
         stage.show();
     }
 
-    public Label labelFactory(String text, Color color, Color BgColor){
+    public Label labelFactory(String text, Color color, Color BgColor, Color BorderColor){
         Label label = new Label(text);
         label.setFont((new Font("Arial",32)));
         label.setTextFill(color);
+
         label.setBackground(new Background
         (new BackgroundFill(BgColor, CornerRadii.EMPTY, Insets.EMPTY)));
+
         label.setMaxWidth(Double.MAX_VALUE);
         label.setPadding(new Insets(15));
+
         label.setBorder(new Border
-        (new BorderStroke(Color.DARKSLATEGRAY, BorderStrokeStyle.DOTTED, 
-        new CornerRadii(10), BorderStroke.THICK)));
+        (new BorderStroke(BorderColor, BorderStrokeStyle.SOLID, 
+        new CornerRadii(0), BorderStroke.DEFAULT_WIDTHS)));
+        
+        label.setAlignment(Pos.CENTER);
 
         return label;
     }
